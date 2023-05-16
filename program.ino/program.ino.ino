5af3dbf0a53ea1f1ec1e20e89af566513ec2b98a
Daniel Rosel <daniel@alves.world>
2023-05-16 14:32:03 +0200

    fixed up and improved

--- program.ino/program.ino.ino
+++ program.ino/program.ino.ino
@@ -11,6 +11,7 @@
 #include <MD_Parola.h>
 #include <SPI.h>
 
+
 // define the number of devices we have in the chain and the hardware interface
 //#define HARDWARE_TYPE MD_MAX72XX::FC16_HW
 #define HARDWARE_TYPE MD_MAX72XX::GENERIC_HW
@@ -29,16 +30,26 @@ MD_MAX72XX myDisplay = MD_MAX72XX(HARDWARE_TYPE, CS_PIN, MAX_DEVICES);
 // This needs to be an existing endpoint (smartphone hotspot)
 const char* ssid = "Casa Mob";
 const char* password = "zenray123";
-const char* WEBSITE = "<!-- ZENRAY A portable light therapy device that uses researched frequencies to lessen users' anxiety, depression and insomnia symptoms. We will use a device consisting of 9-12 white led lights, controlled by arduino and program 2-3 presets for our MVP that focus on different areas (amusement and stress relief, insomnia relief). The programmes will be based on extensive research in the area that has been carried out since the 70s, our unique selling point is that the device is affordable and portable (similar solutions cost up to 2000 euros on the market right now and are tedious to set up), possibly syncable with music. This is a web interface where the user can select a certain program and a function with that program name will then be called. The user can also see a dashboard of the device's current status and the current program running. The user can also see a log of the device's activity. This is pure HTML, CSS and JS, no PHP or SQL. This website will always be displayed on a phone screen --><html><head> <title>ZenRay</title> <link rel=\"stylesheet\" href=\"style.css\"> <script src=\"https://ajax.googleapis.com/ajax/libs/jquery/3.2.1/jquery.min.js\"></script> <meta name=\"viewport\" content=\"width=device-width, initial-scale=1.0\"></head><body onload=\"getProgram()\"><div class=\"container\"> <div class=\"header\"> <h1>ZenRay</h1> </div><div class=\"content\"> <div class=\"dashboard\"> <h2>Dashboard</h2> <div class=\"dashboard-content\"> <div class=\"dashboard-item\"> <h3>Current program</h3> <p id=\"current-program\">None</p></div><div class=\"dashboard-item\"> <h3>Current status</h3> <p id=\"current-status\">Off</p></div><div class=\"dashboard-item\"> <h3>Log</h3> <p id=\"log\">None</p></div></div></div><div class=\"program\"> <h2>Program</h2> <div class=\"program-content\"> <div class=\"program-item\"> <h3>Programs</h3> <div class=\"program-item-content\"> <button class=\"program-button\" onclick=\"setProgram('amusement')\">Amusement</button> <button class=\"program-button\" onclick=\"setProgram('stress')\">Stress relief</button> <button class=\"program-button\" onclick=\"setProgram('insomnia')\">Insomnia relief</button> </div></div><div class=\"program-item\"> <h3>Functions</h3> <div class=\"program-item-content\"> <button class=\"program-button\" onclick=\"setFunction('on')\">On</button> <button class=\"program-button\" onclick=\"setFunction('off')\">Off</button> </div></div></div></div></div></div><script>function addToLog(message){// add to local storage (add message) function setProgram(program){document.getElementById(\"current-program\").innerHTML=program; // redirect to /runProgram/1 window.location.href=\"/runProgram/\" + program;}function setFunction(func){}function getProgram(){// get program from /getProgram let runningProgram=window.location.href.split(\"/\").pop(); document.getElementById(\"current-program\").innerHTML=runningProgram;}// document on load document.addEventListener(\"DOMContentLoaded\", function(event){getProgram();});</script></body></html><style>/* Simple and minimal style for the web interface of ZenRay */ body{background-color: #f2f2f2; font-family: \"Helvetica Neue\", Helvetica, Arial, sans-serif;}.container{width: 100%; max-width: 800px; margin: 0 auto;}.header{background-color: #333; color: #fff; padding: 20px; text-align: center;}.header h1{margin: 0;}.content{padding: 20px;}.dashboard{background-color: #fff; padding: 20px; margin-bottom: 20px;}.dashboard h2{margin: 0;}.dashboard-content{display: flex; flex-wrap: wrap;}.dashboard-item{flex: 1 1 300px; margin: 10px; padding: 20px; background-color: #f2f2f2;}.dashboard-item h3{margin: 0;}.program{background-color: #fff; padding: 20px;}.program h2{margin: 0;}.program-content{display: flex; flex-wrap: wrap;}.program-item{flex: 1 1 300px; margin: 10px; padding: 20px; background-color: #f2f2f2;}.program-item h3{margin: 0;}.program-item-content{display: flex; flex-wrap: wrap;}.program-button{flex: 1 1 100px; margin: 10px; padding: 20px; background-color: #333; color: #fff; border: none; cursor: pointer;}.program-button:hover{background-color: #555;}</style>";
+const char* WEBSITE ="<html><head> <title>ZenRay</title> <link rel=\"stylesheet\" href=\"style.css\"> <script src=\"https://ajax.googleapis.com/ajax/libs/jquery/3.2.1/jquery.min.js\"></script> <meta name=\"viewport\" content=\"width=device-width, initial-scale=1.0\"></head><script>function addToLog(message){// add to local storage (add message) function setProgram(program){document.getElementById(\"current-program\").innerHTML=program; // redirect to /runProgram/1 window.location.href=\"/runProgram/\" + program;}function setFunction(func){}function getProgram(){// get program from /getProgram let runningProgram=window.location.href.split(\"/\").pop(); document.getElementById(\"current-program\").innerHTML=runningProgram;}// document on load document.addEventListener(\"DOMContentLoaded\", function(event){getProgram();});</script><body onload=\"getProgram()\"><div class=\"container\"> <div class=\"header\"> <h1>ZenRay</h1> </div><div class=\"content\"> <div class=\"dashboard\"> <h2>Dashboard</h2> <div class=\"dashboard-content\"> <div class=\"dashboard-item\"> <h3>Current program</h3> <p id=\"current-program\">None</p></div><div class=\"dashboard-item\"> <h3>Current status</h3> <p id=\"current-status\">Off</p></div><div class=\"dashboard-item\"> <h3>Log</h3> <p id=\"log\">None</p></div></div></div><div class=\"program\"> <h2>Program</h2> <div class=\"program-content\"> <div class=\"program-item\"> <h3>Programs</h3> <div class=\"program-item-content\"> <a href=\"/runProgram/1\"><button class=\"program-button\">Relief</button></a> <a href=\"/runProgram/2\"><button class=\"program-button\">Amusement</button></a> </div></div><div class=\"program-item\"> <h3>Functions</h3> <div class=\"program-item-content\"> <a href=\"/stopProgram\"><button class=\"program-button\">Stop</button></a> </div></div></div></div></div></div></body></html><style>/* Simple and minimal style for the web interface of ZenRay */ body{background-color: #f2f2f2; font-family: \"Helvetica Neue\", Helvetica, Arial, sans-serif;}.container{width: 100%; max-width: 800px; margin: 0 auto;}.header{background-color: #333; color: #fff; padding: 20px; text-align: center;}.header h1{margin: 0;}.content{padding: 20px;}.dashboard{background-color: #fff; padding: 20px; margin-bottom: 20px;}.dashboard h2{margin: 0;}.dashboard-content{display: flex; flex-wrap: wrap;}.dashboard-item{flex: 1 1 300px; margin: 10px; padding: 20px; background-color: #f2f2f2;}.dashboard-item h3{margin: 0;}.program{background-color: #fff; padding: 20px;}.program h2{margin: 0;}.program-content{display: flex; flex-wrap: wrap;}.program-item{flex: 1 1 300px; margin: 10px; padding: 20px; background-color: #f2f2f2;}.program-item h3{margin: 0;}.program-item-content{display: flex; flex-wrap: wrap;}.program-button{flex: 1 1 100px; margin: 10px; padding: 20px; background-color: #333; color: #fff; border: none; cursor: pointer;}.program-button:hover{background-color: #555;}</style>";
 
 
 
 WiFiServer server(80);
 
+#include <cstdlib>
+#include <ctime>
+
+int getRandomInt(int min, int max) {
+  // Generate a random number between min and max inclusive
+  int randomNum = std::rand() % (max - min + 1) + min;
+
+  return randomNum;
+}
 
 // define the amusement program
 // list of arrays (x,y) that will be lit up in a loop
 // the first element is the x coordinate, the second is the y coordinate
+// X[0-7] Y[0-16]
 
 int AMUSEMENT[50][2] = {
   {1,1}, {1,2}, {1,3}, {1,4}, {1,5},
@@ -53,16 +64,65 @@ int AMUSEMENT[50][2] = {
   {1,5}, {2,5}, {3,5}, {4,5}, {5,5}
 };
 
+
+// create a class for a program
+class Program {
+ public:
+  int increment=0;
+  int frequency=3; // Hz
+  void next() {
+    increment += 1;
+    Serial.print(increment);
+    Serial.write(" Was the increment\n");
+    for (int i = 0; i < 50; i++) {
+      int randomX = getRandomInt(0,7);
+      int randomY = getRandomInt(0,16);
+      myDisplay.setPoint(randomX, randomY, !myDisplay.getPoint(randomX, randomY));
+    }
+    increment += 1;
+  }
+  void timeout() {
+    float timeoutTime = 1000/frequency;
+    delay(timeoutTime);
+    // clear the display
+    myDisplay.clear();
+  }
+};
+
+// - all the leds turn on and off (10hz if possible, otherwise 3)
+// Amusement Program
+class AmusementProgram {
+ public:
+  int increment=0;
+  int frequency=10; // Hz
+  void next() {
+    // flash one led at a time
+    // increment the counter
+    // flash all the leds
+    // x[0-7] y[0-16]
+    increment += 1;
+    Serial.print(increment);
+    Serial.write(" Was the increment\n");
+    for (int i = 0; i < 8; i++) {
+      for (int j = 0; j < 16; j++) {
+        myDisplay.setPoint(i, j, true);
+      }
+    }
+    increment += 1;
+  }
+  void timeout() {
+    float timeoutTime = 1000/frequency;
+    delay(timeoutTime);
+    // clear the display
+    myDisplay.clear();
+  }
+};
+
+
+
+int currentProgram = -1;
 bool RUNNING = false;
-int increment = 0;
-void sendData() {
-  // go sending the data to the device
-  // go over the array and light up the leds
-  // for each element in the array, light up the corresponding led
-  Serial.print(increment);
-  myDisplay.setPoint(increment, 3, true);
-  delay(1000);
-}
+
 
 bool runProgram(){
   RUNNING = true;
@@ -75,19 +135,11 @@ bool stopProgram(){
   RUNNING = false;
   Serial.println("stopping program");
   // clear the display and turn off the lights
-
+  myDisplay.clear();
+  currentProgram = -1;
   return RUNNING;
 }
 
-#include <cstdlib>
-#include <ctime>
-
-int getRandomInt(int min, int max) {
-    // Generate a random number between min and max inclusive
-    int randomNum = std::rand() % (max - min + 1) + min;
-
-    return randomNum;
-}
 
 
 
@@ -120,15 +172,14 @@ void onConnect() {
   server.begin();
   Serial.println("HTTP server started");
   CONNECTED = true;
+  myDisplay.begin();
 }
 
 void displayGreet() {
   // initialize the display
-  myDisplay.begin();
-
 
   // create a line along the first and second dispay
-  // X [0-7] Y[0-16]
+  // X[0-7] Y[0-16]
   for (int i = 0; i < 5000; i++) {
     int randX = getRandomInt(0,7);
     int randY = getRandomInt(0,16);
@@ -137,6 +188,9 @@ void displayGreet() {
   }
 }
 
+
+Program program;
+AmusementProgram amusementProgram;
 void loop() {
 
   if (WiFi.status() != WL_CONNECTED) {
@@ -152,18 +206,19 @@ void loop() {
     onConnect();
   }
 
-
-
-
-  if (RUNNING) {
-    if (increment < 8) {
-      increment += 1;
-      Serial.print(increment);
-      Serial.write(" Was the increment\n");
-    } else {
-      stopProgram();
+  if (RUNNING && (currentProgram != -1)) {
+    if (currentProgram == 1) {
+      // run the program
+      program.next();
+      program.timeout();
+    }
+    else if (currentProgram == 2) {
+      // run the amusement program
+      amusementProgram.next();
+      amusementProgram.timeout();
     }
   }
+
   WiFiClient client = server.available();
   if (client) {
     Serial.println("new client");
@@ -188,8 +243,10 @@ void loop() {
         }
 
         if (currentLine.endsWith("GET /runProgram/1")) {
+          currentProgram = 1;
           runProgram();
         } else if (currentLine.endsWith("GET /runProgram/2")) {
+          currentProgram = 2;
           runProgram();
         } else if (currentLine.endsWith("GET /stopProgram")) {
           stopProgram();
